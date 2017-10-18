795b8b00ddb33644a27a586b35ba30ea497b0197
WaysToGo <readytohackk@gmail.com>
2017-10-18 19:50:13 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var myApp = angular.module("myApp", []);
+myApp.controller("wikiController", function($scope, $http) {
+  $scope.search = function() {
+    $scope.result = [];
+    $http({
+      url:
+        "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&search=" +
+        $scope.input +
+        "&format=json"
+    }).then(
+      function(success) {
+        var length=success.data[1].length;
+        
+        for (var i = 0; i <length ; i++) {
+         
+          $scope.result.push({
+            name: success.data[1][i],
+            title: success.data[2][i],
+            link: success.data[3][i]
+          
+          });
+        }
+      },
+      function(error) {
+        console.log("check api");
+      }
+    ).then;
+    $scope.random = function() {
+      window.open("https://en.wikipedia.org/wiki/Special:Random");
+    };
+  };
+});
